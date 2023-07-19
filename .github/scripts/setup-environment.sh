40b3af51de9cea93fc2a5f069b24ad2606682c93
Wiktoria Kuna <wkuna@antmicro.com>
2023-07-19 08:48:27 +0200

    Rework CI
    Remove environment-setup.sh script and delegate fetching dependencies to
    yml.

--- .github/scripts/setup-environment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get --allow-releaseinfo-change update && sudo apt-get install -y --no-install-recommends apt-transport-https curl gnupg graphicsmagick-imagemagick-compat graphicsmagick-imagemagick-compat default-jdk default-jre
-echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list:wq
-echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
-curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
-sudo apt-get update && sudo apt-get install -y sbt
